dc44725a582aa93c3a9c85d94f4f713e6e317a3c
Vachaspati <vachasp2@taubh2.campuscluster.illinois.edu>
2015-06-17 02:12:20 -0500

    Add support for various nj methods

--- pynjst.py
+++ pynjst.py
@@ -2,21 +2,66 @@ import dendropy
 import numpy as np
 import sys
 import subprocess
+import tempfile
+import os
+from numpy import ma
 
-fnj_exec='fnj'
 
-def nj(m):
-    q = np.zeros(m.shape)
-    n = m.shape[0]
-    q = (n - 2)
+nj_exec='phydstar'
 
-def njst(tl):
+
+def phydstar(nj, fname):
+    p = subprocess.Popen(['bash', nj_exec, '-d', nj, '-i', fname])
+    p.wait()
+    tree = open(fname + '_' + nj.lower() + '.t').read()
+    return tree
+
+def bionj(fname):
+    return phydstar('BioNJ', fname)
+def mvr(fname):
+    return phydstar('MVR', fname)
+def nj(fname):
+    return phydstar('NJ', fname)
+def unj(fname):
+    return phydstar('UNJ', fname)
+def fastme(fname):
+    print fname + '_fastme.t'
+    p = subprocess.Popen(['fastme', '-i', fname, '-o', fname + '_fastme.t'])
+    p.wait()
+    tree = open(fname + '_fastme.t').read()
+    return tree    
+def ninja(fname):
+    print open(fname).read()
+    p = subprocess.Popen(['ninja', '--in_type', 'd', fname], stdout=subprocess.PIPE)
+    tree = p.stdout.read()
+    return tree
+
+
+import time
+def njst(tl, method, fname = None):
+    if fname:
+        try: 
+            if len(open(fname).read()):
+                start = time.time()
+                tree = method(fname)
+                print "Time to generate tree", time.time() - start, fname
+                return tree
+        except IOError:
+            pass
+
+
+    start = time.time()
+    tl = dendropy.TreeList.get_from_path(tl, 'newick')
+    print "Time to read trees: ", time.time() - start, len(tl), len(tl.taxon_set)
+
+        
+    start = time.time()
     taxindices = dict([(j, i) for i, j in enumerate(tl.taxon_set)])
     countmat = np.zeros((len(tl.taxon_set), len(tl.taxon_set)))
     njmat = np.zeros((len(tl.taxon_set), len(tl.taxon_set)))
     for t in tl:
         for e in t.get_edge_set():
-            e.length=1
+            e.length=1 * float(len(t.taxon_set))/float(len(t.leaf_nodes()))
         m = dendropy.treecalc.PatristicDistanceMatrix(t)
         for x in t.leaf_nodes():
             xi = taxindices[x.taxon]
@@ -24,26 +69,41 @@ def njst(tl):
                 yi = taxindices[y.taxon]
                 countmat[xi, yi] += 1
                 njmat[xi, yi] += m(x.taxon, y.taxon)
-    njmat /= countmat
     
-    lines = []
+    njmat = ma.array(njmat, mask = (countmat == 0))
+    countmat = ma.array(countmat, mask = (countmat == 0))
 
-    
+    njmat /= countmat
+    print "Time to construct matrix", time.time() - start, len(tl), len(tl.taxon_set)
+    lines = []
+    start = time.time()
     for i in taxindices:
         vals = ' '.join([str(njmat[taxindices[i], taxindices[j]]) for j in taxindices])
         lines.append('     '.join([i.label, vals]))
-    distmat = '\n'.join(lines)
+    distmat = '\n'.join([i.replace('--' ,'-99.0') for i in lines])
+    tmp = None
+    if fname == None:
+        tmpfd, fname = tempfile.mkstemp()
+        tmp = os.fdopen(tmpfd, 'w')
+    tmp = open(fname, 'w')
+    tmp.write(str(len(taxindices)))
+    tmp.write('\n')
+    tmp.write(distmat)
+    tmp.write('\n')
+    tmp.close()    
+    print "Time to write matrix", time.time() - start, len(tl), len(tl.taxon_set)
 
-    p = subprocess.Popen([fnj_exec, '-I', 'phylip'], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-    p.stdin.write(str(len(taxindices)))
-    p.stdin.write('\n')
-    p.stdin.write(distmat)
-    p.stdin.close()
-    tree = [i for i in p.stdout.readlines() if '<newick>' in i][0].strip().replace('<newick>', '').replace('</newick>', '')
-    print tree
-#    print njmat
-
+    start = time.time()
+    
+    tree = method(fname)
+    print "Time to generate tree", time.time() - start, len(tl), len(tl.taxon_set)
+    return tree
 
 if __name__ == '__main__':
-    tl = dendropy.TreeList.get_from_path(sys.argv[1], 'newick')
-    njst(tl)
+    method = globals()[sys.argv[1]]
+    tl = sys.argv[2]
+    fname = None
+    if len(sys.argv) > 4:
+        fname = sys.argv[4]
+    tree = njst(tl, method, fname)
+    open(sys.argv[3], 'w').write(tree)
