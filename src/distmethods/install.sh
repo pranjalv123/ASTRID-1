cb704926f7ea1fa83f70bf9c27529e8d48abd5ec
Pranjal Vachaspati <pranjal@pranj.al>
2015-11-25 20:49:05 -0500

    remove some debug code

--- src/distmethods/install.sh
+++ src/distmethods/install.sh
@@ -3,8 +3,7 @@ cd "$( dirname "${BASH_SOURCE[0]}" )"
 
 echo "Installing FastME..."
 # Install FastME
-#wget http://www.ncbi.nlm.nih.gov/CBBresearch/Desper/FastME.tar -O FastME.tar
-cp ~/FastME2.tar .
+wget http://www.ncbi.nlm.nih.gov/CBBresearch/Desper/FastME.tar -O FastME.tar
 tar xvf FastME.tar
 mv program FastME
 cd FastME
@@ -16,8 +15,7 @@ echo "Installed FastME!"
 
 echo "Installing FastME 2..."
 # Install FastME
-#wget http://www.atgc-montpellier.fr/download/sources/fastme/fastme-2.1.4.tar.gz -O FastME2.tar.gz
-cp ~/FastME2.tar.gz .
+wget http://www.atgc-montpellier.fr/download/sources/fastme/fastme-2.1.4.tar.gz -O FastME2.tar.gz
 tar xvf FastME2.tar.gz
 cd fastme-2.1.4
 ./configure
@@ -29,8 +27,7 @@ echo "Installed FastME 2!"
 
 echo "Installing PhyD*..."
 # Install PhyDStar
-#wget http://www.atgc-montpellier.fr/download/binaries/phyd/PhyDstar.tar.gz -O PhyDstar.tar.gz
-cp ~/PhyDstar.tar.gz .
+wget http://www.atgc-montpellier.fr/download/binaries/phyd/PhyDstar.tar.gz -O PhyDstar.tar.gz
 tar xvf PhyDstar.tar.gz
 cp PhyDstar/PhyDstar.jar .
 echo "Installed PhyD*!"
